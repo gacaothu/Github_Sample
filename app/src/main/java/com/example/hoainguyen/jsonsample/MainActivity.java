8c6b3b58bb29331a0c78ce2d7a66418c2c0421fb
Jackie Nguyen <neverbackdown.1988@gmail.com>
2016-04-04 08:47:55 +0700

    Upload source code

---
+++ app/src/main/java/com/example/hoainguyen/jsonsample/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.hoainguyen.jsonsample;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    //private ProgressDialog pDialog;
+    private ListView listView;
+
+    // JSON Node names
+    private static final String TAG_CONTACTS = "contacts";
+    private static final String TAG_ID = "id";
+    private static final String TAG_NAME = "name";
+    private static final String TAG_EMAIL = "email";
+    private static final String TAG_ADDRESS = "address";
+    private static final String TAG_GENDER = "gender";
+    private static final String TAG_PHONE = "phone";
+    private static final String TAG_PHONE_MOBILE = "mobile";
+    private static final String TAG_PHONE_HOME = "home";
+    private static final String TAG_PHONE_OFFICE = "office";
+
+    // contacts JSONArray
+    JSONArray contacts = null;
+
+    // Hashmap for ListView
+    ArrayList<HashMap<String, String>> contactList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.listView);
+
+        parseJson();
+
+        ListAdapter adapter = new SimpleAdapter(
+                MainActivity.this, contactList,
+                R.layout.list_item, new String[]{TAG_NAME, TAG_EMAIL,
+                TAG_PHONE_MOBILE}, new int[]{R.id.name,
+                R.id.email, R.id.mobile});
+
+        listView.setAdapter(adapter);
+    }
+
+    private void parseJson() {
+        String json = loadDataFromFile();
+
+        if (json != null) {
+            try {
+                JSONObject jsonObj = new JSONObject(json);
+
+                // Getting JSON Array node
+                contacts = jsonObj.getJSONArray(TAG_CONTACTS);
+
+                // looping through All Contacts
+                for (int i = 0; i < contacts.length(); i++) {
+                    JSONObject c = contacts.getJSONObject(i);
+
+                    String id = c.getString(TAG_ID);
+                    String name = c.getString(TAG_NAME);
+                    String email = c.getString(TAG_EMAIL);
+                    String address = c.getString(TAG_ADDRESS);
+                    String gender = c.getString(TAG_GENDER);
+
+                    // Phone node is JSON Object
+                    JSONObject phone = c.getJSONObject(TAG_PHONE);
+                    String mobile = phone.getString(TAG_PHONE_MOBILE);
+                    String home = phone.getString(TAG_PHONE_HOME);
+                    String office = phone.getString(TAG_PHONE_OFFICE);
+
+                    // tmp hashmap for single contact
+                    HashMap<String, String> contact = new HashMap<String, String>();
+
+                    // adding each child node to HashMap key => value
+                    contact.put(TAG_ID, id);
+                    contact.put(TAG_NAME, name);
+                    contact.put(TAG_EMAIL, email);
+                    contact.put(TAG_PHONE_MOBILE, mobile);
+
+                    // adding contact to contact list
+                    contactList.add(contact);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        } else {
+            //Log.e("ServiceHandler", "Couldn't get any data from the url");
+        }
+    }
+
+    private String loadDataFromFile() {
+        String json = null;
+        try {
+            InputStream is = getApplication().getAssets().open("myfile.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            json = new String(buffer, "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
+}
